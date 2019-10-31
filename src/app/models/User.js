3b3bef8634f24ea09d84f69e01904a478dcb13bb
lucasbuttow <lucas_buttow@outlook.com>
2019-10-30 21:22:44 -0300

    Setting some mongoose configs

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,9 +1,8 @@
 import bcrypt from 'bcryptjs';
 // import 'database';
 
-import database from '../../database';
+import {mongoose} from '../../database';
 
-import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   name: {
--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,19 @@
 import databaseConfig from '../config/database';
 import mongoose from 'mongoose';
 
+
 class Database {
   constructor() {
     this.init();
   }
 
   init() {
-    // this.connection = new Sequelize(databaseConfig);
+    mongoose.set('useCreateIndex', true)
     mongoose.connect('mongodb://localhost:27017/violentapi', { useNewUrlParser:true, useUnifiedTopology: true });
     mongoose.Promise = global.Promise;
   }
 }
 
-export default new Database();
+const db = new Database();
+
+export {db as default, mongoose }
